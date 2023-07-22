ad7be34997181cf37b6c2e883e6b98c044b1aa15
M-Janaki <janaki10nandhu@gmail.com>
2023-07-22 12:10:37 +0530

    I am janaki

--- app1/src/app1/App1.java
+++ app1/src/app1/App1.java
@@ -2,9 +2,8 @@
 package app1;
 public class App1 {
     public static void main(String[] args) {
-        System.out.println("HI");
         System.out.println("hello world");
-        System.out.println("welcome");
+        System.out.println("HI");
     }
     
 }
